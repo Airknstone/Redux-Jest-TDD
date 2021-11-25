0df9a1775df05a14aa2a7ca416a5e47ec4666d36
Arkenstone <trejo.allan@yahoo.com>
2021-11-24 21:37:04 -0800

    Updated Store

--- src/counter.spec.js
+++ src/counter.spec.js
@@ -17,9 +17,30 @@ describe('Counter', () => {
   });
 });
 
+//This is the API of Redux Store
 describe('Sheep counting store', () => {
   it('should return the state', () => {
     const state = store.getState();
     expect(state.count).toEqual(0);
+  });
+  it('should dispatch actions and update the state', () => {
+    const action = IncreaseCount();
+    store.dispatch(action);
+    expect(store.getState().count).toEqual(1);
+  });
+  it('should call the subscribers when the store data changes', () => {
+    const listener = jest.fn();
+    store.subscribe(listener);
+    const action = IncreaseCount();
+    store.dispatch(action);
+    expect(listener).toHaveBeenCalled();
+  });
+  it('should unsubscribe from store', () => {
+    const listener = jest.fn();
+    const unsubscribe = store.subscribe(listener);
+    const action = IncreaseCount();
+    unsubscribe();
+    store.dispatch(action);
+    expect(listener).not.toHaveBeenCalled();
   });
 });
